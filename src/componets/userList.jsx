107b9298cb340a6fb40e6dc83ce4625d1548c8b9
Mahadevan Reji <rmahadevan574@gmail.com>
2025-05-09 21:18:24 +0530

    fix: adjust role display logic for mobile view in UserList component

--- src/componets/userList.jsx
+++ src/componets/userList.jsx
@@ -117,11 +117,11 @@ export default function UserList() {
             <div className={styles.roles}>
               <div className={styles.roleTagContainer}>
                 {user.roles.map((role, idx) => {
-                    if (isMobile && idx===2) {
-                        const roleCount = user.roles.length - 2;
+                    if (isMobile && idx===1) {
+                        const roleCount = user.roles.length - 1;
                         return <span className={styles.roleTag} key={idx}>+{roleCount}</span>
                     }
-                    if (isMobile && idx > 2) {
+                    if (isMobile && idx > 1) {
                         return null;
                     }
                   return (
